f1c0661fd87272903a74945379a59b7e3d07f2df
sophiadhm <sophiadhm@gmail.com>
2026-03-26 15:42:56 -0400

    Introduce bug for CD demo

--- src/inventory.py
+++ src/inventory.py
@@ -25,8 +25,8 @@ def reduce_stock(item_id: str, quantity: int) -> bool:
     if current < quantity:
         return False
     _stock[item_id] = current - quantity
-    return True
-    # return _stock[item_id]
+    # return True
+    return _stock[item_id]
     # returns 0 when stock hits zero
 
 
